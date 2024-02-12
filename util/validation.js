4a4515b4cac343be41ed541e2e44991dba802528
HasanliHM <hacaga.hasanli@kapitalbank.az>
2024-02-12 12:39:41 +0400

    Remove files that are now in .gitignore

--- util/validation.js
+++
@@ -1,6 +0,0 @@
-import { ValidationError } from './errors.js';
-
-export function validateNotEmpty(text, errorMessage) {
-  if (!text || text.trim().length === 0)
-    throw new ValidationError(errorMessage);
-}
