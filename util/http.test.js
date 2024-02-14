4b8554305229c01012a77fd28e61d8263a4e014b
HasanliHM <hacaga.hasanli@kapitalbank.az>
2024-02-14 12:11:44 +0400

    adding tests for checking instance of error and rejected side

--- util/http.test.js
+++ util/http.test.js
@@ -2,12 +2,14 @@ import { describe, expect, it, vi } from 'vitest';
 
 import { sendDataRequest } from './http';
 
+import { HttpError } from './errors';
+
 const testResponseData = { testKey: 'testData' };
 
 const testFetch = vi.fn((url, options) => {
   return new Promise((resolve, reject) => {
-    if(typeof options.body !== 'string') {
-        return reject('Not a string.')
+    if (typeof options.body !== 'string') {
+      return reject('Not a string.');
     }
 
     const testReponse = {
@@ -36,12 +38,32 @@ describe('sendDataRequest()', () => {
 
     let errorMessage;
 
-    try{
-        await sendDataRequest(testData)
-    }catch(err){
-        errorMessage = err;
+    try {
+      await sendDataRequest(testData);
+    } catch (err) {
+      errorMessage = err;
     }
 
-    expect(errorMessage).not.toBe('Not a string.')
-  })
+    expect(errorMessage).not.toBe('Not a string.');
+  });
+
+  it('should throw an HttpError in case of non-ok responses', () => {
+    testFetch.mockImplementationOnce((url, options) => {
+      return new Promise((resolve, reject) => {
+        const testReponse = {
+          ok: false,
+          json() {
+            return new Promise((resolve, reject) => {
+              resolve(testResponseData);
+            });
+          },
+        };
+        resolve(testReponse);
+      });
+    });
+
+    const testData = { key: 'test' };
+
+    return expect(sendDataRequest(testData)).rejects.toBeInstanceOf(HttpError);
+  });
 });
