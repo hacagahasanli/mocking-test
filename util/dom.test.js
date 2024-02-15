7873c6a5d1eab98d795f07fed07b39a0262be756
HasanliHM <hacaga.hasanli@kapitalbank.az>
2024-02-15 23:21:47 +0400

    adding some testing for dom.js

--- util/dom.test.js
+++ util/dom.test.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 import { Window } from 'happy-dom';
-import { expect, it, vi } from 'vitest';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 
 import { showError } from './dom';
 
@@ -12,18 +12,38 @@ const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString();
 const window = new Window();
 
 const document = window.document;
-document.write(htmlDocumentContent)
-vi.stubGlobal('document', document)
+vi.stubGlobal('document', document);
 
-describe('showError()', () => {
-
+beforeEach(() => {
+    document.body.innerHTML = '';
+    document.write(htmlDocumentContent);
 })
 
-it('should add an error paragraph to the id="errors" element', () => {
+describe('showError()', () => {
+  it('should add an error paragraph to the id="errors" element', () => {
     showError('Test');
 
     const errorsEl = document.getElementById('errors');
     const errorParagraph = errorsEl.firstElementChild;
 
-    expect(errorParagraph).not.toBeNull()
+    expect(errorParagraph).not.toBeNull();
+  });
+
+  it('should not contain an error paragraph initially', () => {
+    const errorsEl = document.getElementById('errors');
+    const errorParagraph = errorsEl.firstElementChild;
+
+    expect(errorParagraph).toBeNull();
+  })
+
+  it('should output the provided message in the error paragraph', () => {
+    const textErrorMessage = 'test'
+
+    showError(textErrorMessage);
+
+    const errorsEl = document.getElementById('errors');
+    const errorParagraph = errorsEl.firstElementChild;
+
+    expect(errorParagraph.textContent).toBe(textErrorMessage);
+  })
 });
