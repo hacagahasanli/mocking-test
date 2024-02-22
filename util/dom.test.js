6aff0579353d61ca38fc2342c5557d9a850fa137
HasanliHM <hacaga.hasanli@kapitalbank.az>
2024-02-22 22:06:25 +0400

    order changed

--- util/dom.test.js
+++ util/dom.test.js
@@ -6,10 +6,10 @@ import { beforeEach, describe, expect, it, vi } from 'vitest';
 
 import { showError } from './dom';
 
+const window = new Window();
+
 const htmlDocPath = path.join(process.cwd(), 'index.html');
 const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString();
-
-const window = new Window();
 
 const document = window.document;
 vi.stubGlobal('document', document);
